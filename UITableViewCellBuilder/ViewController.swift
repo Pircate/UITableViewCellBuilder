c297f7a0495543caf151cb23c4c4c0216ef11f4b
Pircate <gao497868860@163.com>
2018-09-15 11:00:42 +0800

    Initial Commit

---
+++ UITableViewCellBuilder/ViewController.swift
@@ -0,0 +1,28 @@
+// 
+//  ViewController.swift
+//  UITableViewCellBuilder
+//
+//  Created by Pircate on 2018/9/15
+//  Copyright © 2018年 Pircate. All rights reserved.
+//
+//  Email: gao497868860@163.com
+//  Github: https://github.com/Pircate
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ UITableViewCellBuilderTests/UITableViewCellBuilderTests.swift
@@ -0,0 +1,39 @@
+// 
+//  UITableViewCellBuilderTests.swift
+//  UITableViewCellBuilderTests
+//
+//  Created by Pircate on 2018/9/15
+//  Copyright © 2018年 Pircate. All rights reserved.
+//
+//  Email: gao497868860@163.com
+//  Github: https://github.com/Pircate
+//
+
+import XCTest
+@testable import UITableViewCellBuilder
+
+class UITableViewCellBuilderTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
